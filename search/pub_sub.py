71b7fe1b66c6e9fdbc18584738c8430d87231f04
Louis Beaumont <louis.beaumont@gmail.com>
2023-01-09 22:03:12 +0100

    0.2.2: fix: test

--- search/pub_sub.py
+++ search/pub_sub.py
@@ -6,11 +6,11 @@ import json
 project_id = "obsidian-ai"
 topic_id = "enrich_index"
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(project_id, topic_id)
 
 
 def enrich_doc(docs_id: List[str], namespace: str):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
     publisher.publish(
         topic_path, json.dumps({"ids": docs_id, "namespace": namespace}).encode("utf-8")
     )
