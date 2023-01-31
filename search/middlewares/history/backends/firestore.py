5c97f5454191f7ad70e899def0b459f11ef60100
Louis Beaumont <louis.beaumont@gmail.com>
2023-01-31 09:36:52 +0100

    fix: pass properly firestore client to plugin

--- search/middlewares/history/backends/firestore.py
+++ search/middlewares/history/backends/firestore.py
@@ -1,3 +1,4 @@
+import asyncio
 from itertools import groupby
 from typing import Awaitable, Callable, Optional
 from . import BaseBackend
@@ -14,12 +15,16 @@ class FirestoreBackend(BaseBackend):
         can_log: Optional[Callable[[Client, str, str, Scope], Awaitable[Optional[str]]]] = None,
     ):
         self._firestore = firestore
+
+        if not asyncio.iscoroutinefunction(can_log):
+            raise ValueError(f"invalid can_log function, it must be a coroutine: {can_log}")
+
         self._can_log = can_log
 
     async def can_log(self, user: str, group: str, scope: Scope) -> Optional[str]:
         if not self._can_log:
             return None
-        return await self._can_log(user, group, scope)
+        return await self._can_log(self._firestore, user, group, scope)
 
     async def log(self, user: str, group: str, scope: Scope):
         """log the request in the history"""
