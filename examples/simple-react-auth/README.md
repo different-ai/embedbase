1c7fb96e3fe6839bc1caeba73ed4d91d9588e75b
Louis Beaumont <louis.beaumont@gmail.com>
2023-02-01 15:41:25 +0000

    feat: add authenticated example with React + TS + Firebase

---
+++ examples/simple-react-auth/README.md
@@ -0,0 +1,8 @@
+## simple-react
+
+This is a simple React app that is using **Embedbase** in an authenticated way.
+
+1. [Get your Firebase config](https://console.firebase.google.com/u/0/project/obsidian-ai/settings/general/web:NjRmMWIwZDAtMmE3OC00YTM5LThlMjItYzcxZWUzM2I2NjQ5) and add it to the `src/fb.json` file 
+2. `npm i`
+3. `npm start`
+
---
+++ examples/simple-react-auth/src/App.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// To apply the default browser preference instead of explicitly setting it.
+// firebase.auth().useDeviceLanguage();
+const hashCode = (s: string) => s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0)
+
+// read firebase config from fb.json
+const firebaseConfig = require('./fb.json');
+const app = initializeApp(firebaseConfig);
+
+function App() {
+  const [search, setSearch] = React.useState('');
+  const [documents, setDocuments] = React.useState<{content: string; color: string}[]>([]);
+  const [idToken, setIdToken] = React.useState<string | null>(null);
+  console.log("firebaseConfig", firebaseConfig)
+
+  const handleSearchBarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+  const firebaseAuth = async () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    const {user} = await signInWithPopup(auth, provider)
+    const idToken = await user.getIdTokenResult();
+    setIdToken(idToken.token);
+  }
+  const colourizeDocuments = () => {
+    if (!search) return;
+    fetch('http://localhost:8000/v1/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${idToken}`,
+      },
+      body: JSON.stringify({
+        vault_id: 'dev',
+        query: search,
+      }),
+    }).then((res) => res.json()).then((res) => {
+      // get the highest score in res.similarities
+      const topOne = res.similarities.reduce((a: any, b: any) => a.score > b.score ? a : b);
+      // set to the document found in documents
+      // red if close enough otherwise black
+      setDocuments(documents.map((document) => {
+          return { content: document.content, color: 
+            (document.content === topOne.document_content && 
+              topOne.score > 0.8) ? 'red' : 'black' };
+      }));
+    }).catch((err) => alert(err));
+  };
+  const addDocument = () => {
+    // hash of search bar + time
+    const id = hashCode(search + Date.now().toString());
+    fetch('http://localhost:8000/v1/search/refresh', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${idToken}`,
+      },
+      body: JSON.stringify({
+        vault_id: 'dev',
+        documents: [{
+          document_id: id,
+          document_path: id,
+          document_content: search,
+          document_tags: [],
+          document_embedding_format: search,
+        }],
+      }),
+    }).then(() => setDocuments([...documents, { content: search, color: 'black' }]))
+      .catch((err) => alert(err));
+  }
+  const clearIndex = () => {
+    // /v1/search/clear
+    fetch('http://localhost:8000/v1/search/clear', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${idToken}`,
+      },
+      body: JSON.stringify({
+        vault_id: 'dev',
+      }),
+    }).then(() => setDocuments([])).catch((err) => alert(err));
+  }
+
+  return (
+    <>
+      {/* a list of items centered horizontally and vertically */}
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+        display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {/* a search bar centered horizontally and vertically */}
+        <input type="text" value={search} onChange={handleSearchBarChange}
+          style={{ width: '100%', maxWidth: '500px', height: '50px', fontSize: '20px' }}
+        />
+        {/* a list of documents */}
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          {documents.map((document, i) => (
+            <li key={i} style={{ color: document.color,
+              fontSize: '20px', margin: '10px 0' }}
+            >{document.content}</li>
+          ))}
+        </ul>
+      </div>
+      <button style={{ position: 'absolute', top: '40%', right: '20%' }}
+        onClick={() => firebaseAuth().catch((err) => alert(err))}>
+        Login with Google
+      </button>
+      {idToken && <button
+        style={{ position: 'absolute', top: '35%', right: '20%'}}
+        // on hover show id token
+        onClick={() => alert(idToken)}
+      >Click me to see id token</button>}
+      <button style={{ position: 'absolute', top: '30%', right: '20%' }}
+        onClick={() => {
+          addDocument();
+          // clear the search bar
+          setSearch('');
+        }}>
+        Add to index
+      </button>
+      <button style={{ position: 'absolute', top: '20%', right: '20%' }}
+        onClick={colourizeDocuments}>
+        Colourize documents
+      </button>
+      <button style={{ position: 'absolute', top: '10%', right: '20%' }}
+        onClick={clearIndex}>
+        Clear index
+      </button>
+    </>
+  );
+}
+
+export default App;
