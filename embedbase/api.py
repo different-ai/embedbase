efb99fc7cd8d72d8bd4c233678950752b82b5a40
Louis Beaumont <louis.beaumont@gmail.com>
2023-02-16 15:28:52 +0000

    0.6.7: fix middleware logging

--- embedbase/api.py
+++ embedbase/api.py
@@ -52,21 +52,21 @@ if settings.middlewares:
         # and add the first class found to the list
 
         try:
-            logger.info("Importing middleware", m)
+            logger.info(f"Importing middleware {m}")
             segments = m.split(".")
-            logger.debug("Segments", segments)
+            logger.debug(f"Segments {segments}")
             module_name = ".".join(segments[0:-1])
-            logger.debug("Module name", module_name)
+            logger.debug(f"Module name {module_name}")
             class_name = segments[-1]
-            logger.debug("Class name", class_name)
+            logger.debug(f"Class name {class_name}")
             module = __import__(module_name, fromlist=[class_name])
-            logger.debug("Module", module)
+            logger.debug(f"Module {module}")
             dirs = dir(module)
-            logger.debug("Dirs", dirs)
+            logger.debug(f"Dirs {dirs}")
             middleware_class = getattr(module, class_name)
-            logger.debug("Middleware class", middleware_class)
+            logger.debug(f"Middleware class {middleware_class}")
             middlewares.append(Middleware(middleware_class))
-            logger.info("Loaded middleware", m)
+            logger.info(f"Loaded middleware {m}")
         except Exception as e:
             logger.error(f"Error loading middleware {m}: {e}")
 
