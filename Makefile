1bd18e6fe74c12f2ec8e6649a107c6e680c18887
Louis Beaumont <louis.beaumont@gmail.com>
2023-03-09 13:30:34 +0000

    feat: firebase auth unit test

--- Makefile
+++ Makefile
@@ -30,7 +30,8 @@ test: ## [Local development] Run tests with pytest.
 	python3 -m pytest -s test_end_to_end.py::test_ignore_document_that_didnt_change; \
 	python3 -m pytest -s test_end_to_end.py::test_save_clear_data; \
 	python3 -m pytest -s test_end_to_end.py::test_health_properly_forward_headers; \
-	python3 -m pytest -s test_end_to_end.py::test_insert_large_documents
+	python3 -m pytest -s test_end_to_end.py::test_insert_large_documents; \
+	python3 -m pytest -s test_auth.py::test_enable_firebase_auth
 # TODO in api.py
 # python3 -m pytest -s test_end_to_end.py::test_adding_twice_the_same_data_is_ignored
 	@echo "Done testing"
--- embedbase/test_auth.py
+++ embedbase/test_auth.py
@@ -1,6 +1,23 @@
+import pytest
+from httpx import AsyncClient
 
 from embedbase.firebase_auth import enable_firebase_auth
 
+@pytest.mark.asyncio
+async def test_enable_firebase_auth():
+    from .api import app
 
-def test_enable_firebase_auth():
-    enable_firebase_auth()
+    # before enabling auth, we should be able to make queries
+    # without any authorization header
+    enable_firebase_auth(app)
+
+    # after enabling auth, we should get a 401 error
+    # when not providing an authorization header
+    async with AsyncClient(app=app, base_url="http://localhost:8000") as client:
+        response = await client.post(
+            "/v1/unit_test/search",
+            json={"query": "bob"},
+        )
+        assert response.status_code == 401
+    # when providing an authorization header, we should be able to make queries
+    # TODO: cannot create id token on backend :/
