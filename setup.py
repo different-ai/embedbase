358b50d6bc0fda31c8f9c86db6910f952dd0853b
Louis Beaumont <louis.beaumont@gmail.com>
2023-04-16 17:39:07 +0200

    Release 1.0.2: fix: release wf

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name="embedbase",
     packages=find_packages(),
     include_package_data=True,
-    version="1.0.1",
+    version="1.0.2",
     description="API, SDK & dashboard to easily create, store, and retrieve machine learning embeddings",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
