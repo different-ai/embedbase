114afebd7c2293573e8746ac60596cb88e629af5
Louis Beaumont <louis.beaumont@gmail.com>
2023-04-21 16:12:06 +0200

    chore: whole embedbase is indexed in embedbase at push

--- docs/scripts/sync.ts
+++ docs/scripts/sync.ts
@@ -1,12 +1,23 @@
-const glob = require("glob");
-const fs = require("fs");
+import glob from 'glob'
+import fs from 'fs'
 import { createClient, BatchAddDocument } from 'embedbase-js'
 import { splitText } from 'embedbase-js/dist/main/split';
+import path from 'path';
 
 
-const hash = (t: number): string =>
-    t.toString(36).replace(/[^a-z]+/g, '');
-
+const batch = async (myList: any[], fn: (chunk: any[]) => Promise<any>) => {
+    const batchSize = 100;
+    // add to embedbase by batches of size 100
+    return Promise.all(
+        myList.reduce((acc: BatchAddDocument[][], chunk, i) => {
+            if (i % batchSize === 0) {
+                acc.push(myList.slice(i, i + batchSize));
+            }
+            return acc;
+            // here we are using the batchAdd method to send the documents to embedbase
+        }, []).map(fn)
+    )
+}
 try {
     require("dotenv").config();
 } catch (e) {
@@ -19,44 +30,62 @@ const url = 'https://api.embedbase.xyz'
 const embedbase = createClient(url, apiKey)
 
 const sync = async () => {
-    // read all files under pages/* with .mdx extension
-    // for each file, read the content
-    // TODO: sync whole repository, everything!
-    const documents = glob.sync("pages/**/*.mdx").map((path) => ({
-        path: "https://docs.embedbase.xyz" +
-            path.replace("pages/", "/").replace("index.mdx", "").replace(".mdx", ""),
-        // content of the file
-        data: fs.readFileSync(path, "utf-8")
-    }));
+    const ignored = [
+        "node_modules",
+        "dist",
+        "build",
+        "public",
+        "env",
+        ".next",
+        ".git",
+    ];
+    // read all files under pages/* with .md, .mdx, .ts, .py extension
+    const filePaths = glob.sync("../**/*.{md,mdx,ts,py}")
+        .filter((p) => !ignored.some((i) => p.includes(i)) &&
+            // Check if the path is a file
+            fs.statSync(p).isFile());
+
+    const documents = filePaths
+        .map((p) => ({
+            path:
+                // if the file is under docs/ folder, then
+                // it is docs (docs.embedbase.xyz)
+                p.includes("docs/") ?
+                    path.join(
+                        "https://docs.embedbase.xyz",
+                        p.replace("../", "")
+                            .replace("pages/", "/")
+                            .replace("index.mdx", "")
+                            .replace(".mdx", "")
+                    ) :
+                    // otherwise it is https://github.com/different-ai/embedbase
+                    path.join(
+                        "https://github.com/different-ai/embedbase/blob/main",
+                        p.replace("../", "")
+                    ),
+            // content of the file
+            data: fs.readFileSync(p, "utf-8")
+        }));
     const chunks = []
-    documents.map((document) =>
-        splitText(document.data, { maxTokens: 500, chunkOverlap: 200 }, async ({ chunk, start, end }) => chunks.push({
-            data: chunk,
-            metadata: {
-                path: document.path,
-                start,
-                end
-            }
-        }))
-    )
-    // embedbase-doc-[hash of timestamp]
-    // const datasetId = `embedbase-doc-${hash(new Date().getTime())}`
-    // HACK: implement a way to always use latest version of dataset
+    documents
+        // ignore data with "<|endoftext|>" as it crashes the tokenizer
+        .filter((d) => !d.data.includes("<|endoftext|>"))
+        // irony is that this script itself crash itself :D
+        .map((document) =>
+            splitText(document.data, { maxTokens: 500, chunkOverlap: 200 }, async ({ chunk, start, end }) => chunks.push({
+                data: chunk,
+                metadata: {
+                    path: document.path,
+                    start,
+                    end
+                }
+            }))
+        )
     const datasetId = `embedbase-documentation`
 
     console.log(`Syncing to ${datasetId} ${chunks.length} documents`);
 
-
-    const batchSize = 100;
-    // add to embedbase by batches of size 100
-    return Promise.all(
-        chunks.reduce((acc: BatchAddDocument[][], chunk, i) => {
-            if (i % batchSize === 0) {
-                acc.push(chunks.slice(i, i + batchSize));
-            }
-            return acc;
-        }, []).map((chunk) => embedbase.dataset(datasetId).batchAdd(chunk))
-    )
+    return batch(chunks, (chunk) => embedbase.dataset(datasetId).batchAdd(chunk))
         .then((e) => e.flat())
         .then((e) => console.log(`Synced ${e.length} documents to ${datasetId}`))
         .catch(console.error);
