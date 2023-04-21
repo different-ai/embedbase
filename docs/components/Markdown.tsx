94205e1e4d2efa31864a242c47a66b44b0b673df
Louis Beaumont <louis.beaumont@gmail.com>
2023-04-21 16:55:39 +0200

    fix: syntax highlighting in llm-powered doc, also enable links & footnotes just as dashboard

--- docs/components/Markdown.tsx
+++ docs/components/Markdown.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown'
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
 import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
+import python from 'react-syntax-highlighter/dist/cjs/languages/prism/python'
 import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss'
 import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
 import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown'
@@ -13,6 +14,7 @@ import footnotes from 'remark-footnotes'
 
 SyntaxHighlighter.registerLanguage('tsx', tsx)
 SyntaxHighlighter.registerLanguage('typescript', typescript)
+SyntaxHighlighter.registerLanguage('python', python)
 SyntaxHighlighter.registerLanguage('scss', scss)
 SyntaxHighlighter.registerLanguage('bash', bash)
 SyntaxHighlighter.registerLanguage('markdown', markdown)
