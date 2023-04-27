19288d5f1ab4cf2970e4db4fd88321fb826db8fd
Benjamin Shafii <ben@prologe.io>
2023-04-27 19:40:11 +0200

    feat: use normal sample rate

--- dashboard/sentry.server.config.js
+++ dashboard/sentry.server.config.js
@@ -9,7 +9,7 @@ const ENVIRONMENT = process.env.ENVIRONMENT || process.env.NEXT_PUBLIC_ENVIRONME
 ENVIRONMENT === 'production' && Sentry.init({
   dsn: SENTRY_DSN || 'https://1e6e06fb6e9740b8b9d66e782516631a@o4505073433706496.ingest.sentry.io/4505073435082752',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
