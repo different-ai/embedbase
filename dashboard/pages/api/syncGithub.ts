972a650b260e425ffecb8d8dd6985bd494859b63
Benjamin Shafii <ben@prologe.io>
2023-04-27 14:47:27 +0200

    Revert "fix: dashboard: hack to use syncGithub in edge"
    This reverts commit 0f289a3d855e76db256df44d7b311a555e46ee66.

--- dashboard/pages/api/syncGithub.ts
+++ dashboard/pages/api/syncGithub.ts
@@ -1,74 +1,12 @@
 import { getGithubContent, getRepoName } from "@/lib/github";
 import { createClient } from "embedbase-js";
 import { BatchAddDocument } from "embedbase-js/dist/module/types";
-// import { splitText } from "embedbase-js/dist/main/split";
+import { splitText } from "embedbase-js/dist/main/split";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { batch } from "@/utils/array";
-// @ts-expect-error
-import wasm from "../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module";
-import model from "@dqbd/tiktoken/encoders/cl100k_base.json";
-import { init, Tiktoken } from "@dqbd/tiktoken/lite/init";
 
 const EMBEDBASE_URL = "https://api.embedbase.xyz";
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN!;
-
-export const config = {
-  runtime: 'edge',
-}
-
-
-interface SplitTextChunk {
-    chunk: string
-    start: number
-    end: number
-}
-const MAX_CHUNK_LENGTH = 8191
-const EMBEDDING_ENCODING = 'cl100k_base'
-const CHUNK_OVERLAP = 0
-
-
-async function splitText(
-    text: string,
-    {
-        maxTokens = MAX_CHUNK_LENGTH,
-        chunkOverlap = CHUNK_OVERLAP,
-        encodingName = EMBEDDING_ENCODING,
-    }: { maxTokens?: number; chunkOverlap?: number; encodingName?: any },
-    callback?: (chunk: SplitTextChunk) => void
-): Promise<SplitTextChunk[]> {
-    if (chunkOverlap >= maxTokens) {
-        throw new Error('Cannot have chunkOverlap >= chunkSize')
-    }
-    await init((imports) => WebAssembly.instantiate(wasm, imports));
-
-    const encoding = new Tiktoken(
-        model.bpe_ranks,
-        model.special_tokens,
-        model.pat_str
-    );
-
-    const input_ids = encoding.encode(text)
-    const chunkSize = maxTokens
-
-    let start_idx = 0
-    let cur_idx = Math.min(start_idx + chunkSize, input_ids.length)
-    let chunk_ids = input_ids.slice(start_idx, cur_idx)
-
-    const decoder = new TextDecoder()
-    const chunks = []
-
-    while (start_idx < input_ids.length) {
-        const chunk = decoder.decode(encoding.decode(chunk_ids))
-        const chunkItem = { chunk, start: start_idx, end: cur_idx }
-        chunks.push(chunkItem)
-        callback && callback(chunkItem)
-        start_idx += chunkSize - chunkOverlap
-        cur_idx = Math.min(start_idx + chunkSize, input_ids.length)
-        chunk_ids = input_ids.slice(start_idx, cur_idx)
-    }
-    encoding.free()
-    return chunks
-}
 
 const getApiKey = async (req, res) => {
   // Create authenticated Supabase Client
@@ -116,7 +54,7 @@ export default async function sync(req: any, res: any) {
   const repo = getRepoName(url);
 
   // HACK to create dataset
-  await embedbase.dataset(repo).add('.');
+  embedbase.dataset(repo).add('');
 
   const chunks: BatchAddDocument[] = [];
   githubFiles.forEach((file) =>
