9addaffe3dee807ee3a46f54293dc03265b6bd60
Louis Beaumont <louis.beaumont@gmail.com>
2023-05-22 15:10:14 +0200

    chore(hosted) automate the release/deployment process

--- hosted/bump.py
+++ hosted/bump.py
@@ -2,4 +2,4 @@ import semantic_version
 from version import __version__
 v = semantic_version.Version(__version__)
 v.patch = v.patch + 1
-open("version.py", "w").write('__version__ = "{}"\n'.format(v))
+print(v)
