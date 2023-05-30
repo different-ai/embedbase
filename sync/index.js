6fc9296ec0391ad7968e1f47f23cac9bc6dd2a0f
Benjamin Shafii <ben@prologe.io>
2023-05-30 13:13:40 +0200

    feat: add simple file sync example

---
+++ sync/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const multer = require('multer')
+const { createClient } = require('embedbase-js')
+
+
+
+const upload = multer({ dest: 'uploads/' })
+const app = express()
+
+const url = 'https://api.embedbase.xyz'
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/add-text', upload.single('file'), async (req, res) => {
+  const apiKey = req.body.apiKey
+  const dataset = req.body.dataset
+
+  if (!dataset) {
+    return res.status(401).send('Invalid Dataset')
+  }
+
+
+  if (apiKey.length !== 36) {
+    return res.status(401).send('Invalid API Key')
+  }
+  console.log(apiKey)
+  const embedbase = createClient(url, apiKey)
+  // get api key from header
+  const text = require('fs').readFileSync(req.file.path, 'utf8')
+  const data = await embedbase.dataset(dataset).chunkAndBatchAdd([{
+    data: text,
+    metadata: {
+      path: req.file.originalname
+    }
+  }])
+  res.send(data)
+})
+
+app.listen(3000)
