db296f80b68cfcc58e41b23dbf4c49d0a49649ab
Benjamin Shafii <ben@prologe.io>
2023-06-07 14:21:03 -0700

    feat: add login to navbar

--- dashboard/components/Navbar.tsx
+++ dashboard/components/Navbar.tsx
@@ -24,31 +24,11 @@ export default function Navbar() {
                 </Link>
               </div>
             </div>
-            <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
-              <div className="flex items-center px-6 py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
-                {/* hidden for now */}
-                <div className="hidden w-full">
-                  <label htmlFor="search" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative hidden md:block">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                      <MagnifyingGlassIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <Input className="w-full pl-10 " />
-                  </div>
-                </div>
-              </div>
-            </div>
             <div className="flex items-center md:absolute md:inset-y-0 md:right-0 lg:hidden">
-              {/* Mobile menu button */}
             </div>
             <div className="flex items-center justify-end xl:col-span-4">
-              <Link href="/signup">
-                <SecondaryButton className="ml-3">Signup</SecondaryButton>
+              <Link href="/login">
+                <SecondaryButton className="ml-3">Login</SecondaryButton>
               </Link>
               <Link href="/signup">
                 <PrimaryButton className="ml-3">New App</PrimaryButton>
