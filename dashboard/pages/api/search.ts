401d2bcb93634904387a03218560090c29d5d693
Benjamin Shafii <ben@prologe.io>
2023-06-15 11:38:26 -0700

    feat: add cors to /search

--- dashboard/pages/api/search.ts
+++ dashboard/pages/api/search.ts
@@ -1,20 +1,33 @@
-import { semanticSearch } from '@/utils/vectors';
-import * as Sentry from '@sentry/nextjs';
+import cors from '@/utils/cors'
+import { semanticSearch } from '@/utils/vectors'
+import * as Sentry from '@sentry/nextjs'
 
 export const config = {
   runtime: 'edge',
 }
 
 type SearchRequest = {
-  query: string;
-  datasets_id: string[];
-  top_k?: number;
-  where?: Record<string, any>;
+  query: string
+  datasets_id: string[]
+  top_k?: number
+  where?: Record<string, any>
+}
+
+export const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*',
 }
 
 // 2. Get a context from a dataset
 export default async function buildPrompt(req: Request, res: Response) {
-  const { query, datasets_id, top_k, where } = await req.json() as SearchRequest
+  // The `cors` snippet says the preflight OPTIONS is handled, but it's not stable!
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      headers: CORS_HEADERS,
+    })
+  }
+  const { query, datasets_id, top_k, where } =
+    (await req.json()) as SearchRequest
   if (!query) {
     return new Response(JSON.stringify({ error: 'No query' }), {
       status: 400,
@@ -24,15 +37,26 @@ export default async function buildPrompt(req: Request, res: Response) {
   const userId = req.headers.get('userId')
 
   try {
-    const context = await semanticSearch(userId, datasets_id, query, where, top_k)
-    return new Response(JSON.stringify(context), {
-      status: 200,
-    })
+    const context = await semanticSearch(
+      userId,
+      datasets_id,
+      query,
+      where,
+      top_k
+    )
+
+    return cors(
+      req,
+      new Response(JSON.stringify(context), {
+        status: 200,
+      })
+    )
   } catch (error) {
     console.error(error)
     Sentry.captureException(error)
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
+      headers: CORS_HEADERS,
     })
   }
 }
