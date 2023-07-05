cc3f04c2c4ddeaf60afb820db2c9933a89746015
Louis Beaumont <louis.beaumont@gmail.com>
2023-07-05 15:10:55 -0700

    fix(dashboard): github login was weird

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/datasets',
+        destination: '/signup',
         permanent: false,
       },
     ]
--- dashboard/pages/onboarding/create-api-key.tsx
+++ dashboard/pages/onboarding/create-api-key.tsx
@@ -2,6 +2,7 @@ import { ArrowRightCircleIcon } from '@heroicons/react/20/solid'
 import { useRouter } from 'next/router'
 import {  CreateAPIKeyV2 } from '../../components/APIKeys'
 import { CenteredLayout } from '../../components/Layout'
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 
 const Index = () => {
   const router = useRouter()
@@ -33,5 +34,49 @@ const Index = () => {
     </div>
   )
 }
+
+export const getServerSideProps = async (ctx) => {
+  // Create authenticated Supabase Client
+  const supabase = createPagesServerClient(ctx)
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  const hasApiKey = async () => {
+    try {
+      const { data, status, error } = await supabase
+        .from('api-keys')
+        .select()
+        .eq('user_id', session?.user?.id)
+      if (error && status !== 406) {
+        throw error
+      }
+      return data.length > 0
+    } catch (e) {
+      console.error(e)
+      return false
+    }
+  }
+
+  if (session) {
+    const hasKey = await hasApiKey()
+    if (!hasKey) return
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      initialSession: session,
+      user: session?.user ?? {},
+    },
+  }
+}
+
 
 export default Index
